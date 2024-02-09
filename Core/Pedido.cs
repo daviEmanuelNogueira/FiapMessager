ac0b2a9c54146b1792c235b730685a737d766549
Davi Emanuel <daviemanuel94@gmail.com>
2024-02-09 20:36:32 -0300

    Add project files.

---
+++ Core/Pedido.cs
@@ -0,0 +1,16 @@
+﻿namespace Core;
+
+public class Pedido
+{
+    public int Id { get; set; }
+    public Usuario Usuario { get; set; }
+    public DateTime DataCriacao { get; set; }
+
+    public Pedido(int id, Usuario usuario)
+    {
+        Id = id;
+        Usuario = usuario;
+    }
+
+    public override string ToString() => $"Pedido Id {Id}, Usuário {Usuario.Nome}, Data de criação: {DataCriacao:dd/MM/yyyy}";
+}
